2db2dcda40c805191bc5b356dd5a33da04108b4d
CassieRasugu <cassierasugu@gmail.com>
2023-04-14 14:07:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+//Use a function to fetch data from url//
+async function logJSONData () {
+  const response =
+  await fetch ("http://localhost:3000/films");
+  const jsonData = await response.json ();
+  return jsonData
+}
+displayFilms
+async function displayFilms(){
+  const container = document.getElementById ("container")
+  const data = await logJSONData()
+   console.log (data);
+}
 const buyTicketBtn = document.getElementById("buy-ticket-btn");
 const ticketAvailability = document.getElementById("ticket-availability");
 
@@ -13,3 +26,14 @@ buyTicketBtn.addEventListener("click", function() {
     alert("this show is sold out!");
   }
 });
+// update the number number of the tickets //
+async function tickets_sold(id,tickets_sold){
+await fetch(`http://localhost:3000/films/${id}`,
+{
+      method: "PATCH",
+      body: JSON.stringify({tickets_sold:tickets_sold-1}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+})
+}
